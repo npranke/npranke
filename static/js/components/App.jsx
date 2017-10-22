04173053f3435c25d4f474339736a7b6e0c8581d
npranke <nicole@npranke.io>
2017-10-21 18:43:18 -0600

    Add eslint rule "semi": ["error", "never"] for no semicolons

--- static/js/components/App.jsx
+++ static/js/components/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
     return (
         <div className="text">
-            Hello frontend world!
+            Hello frontend world, hello!
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
--- static/js/components/__tests__/App.test.jsx
+++ static/js/components/__tests__/App.test.jsx
@@ -1,25 +1,25 @@
-import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, { shallow } from 'enzyme';
-import React from 'react';
+import Adapter from 'enzyme-adapter-react-16'
+import Enzyme, { shallow } from 'enzyme'
+import React from 'react'
 
-import App from '../App';
+import App from '../App'
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() })
 
 describe('App', () => {
     test('contains expected message', () => {
-        const app = shallow(<App />);
+        const app = shallow(<App />)
 
         expect(
             app.find('.text').text(),
-        ).toEqual('Hello frontend world!');
-    });
-});
+        ).toEqual('Hello frontend world, hello!')
+    })
+})
 
 describe('App snapshot', () => {
     test('matches snapshot', () => {
-        const app = shallow(<App />);
+        const app = shallow(<App />)
 
-        expect(app).toMatchSnapshot();
-    });
-});
+        expect(app).toMatchSnapshot()
+    })
+})
