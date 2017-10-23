718f24d823dd5d833052f54a8c81f0c81f6804b2
npranke <nicole@npranke.io>
2017-10-23 15:36:18 -0600

    Update messages in prod deploy script

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -34,66 +34,66 @@ echo -n "Checking that current branch is master... "
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [[ $BRANCH != "master" ]]
 then
-    echo "${ALRIGHT_MSG}"
+    echo "${NOT_ALRIGHT_MSG}"
     echo -e "Branch is ${MAGENTA}${BRANCH}${NORMAL}, should be master. Deploy aborted.\n"
     exit 1
 else
-    echo "${NOT_ALRIGHT_MSG}"
+    echo "${ALRIGHT_MSG}"
 fi
 
 echo -n "Checking for uncommitted changes... "
 CHANGES=$(git diff-index --name-only HEAD -- && git ls-files --others --exclude-standard)
 if [[ $CHANGES != "" ]]
 then
-    echo "${ALRIGHT_MSG}"
+    echo "${NOT_ALRIGHT_MSG}"
     echo -e "There are uncommitted changes, see: \n${MAGENTA}${CHANGES}${NORMAL}\nDeploy aborted.\n"
     exit 1
 else
-    echo "${NOT_ALRIGHT_MSG}"
+    echo "${ALRIGHT_MSG}"
 fi
 
 echo -n "Pulling with fast forward only from origin... "
 PULL_RESULT=$(git pull --ff-only origin master 2>&1)
 if [[ $? != 0 ]]
 then
-    echo "${ALRIGHT_MSG}"
+    echo "${NOT_ALRIGHT_MSG}"
     echo -e "Error pulling, see: \n${MAGENTA}${PULL_RESULT}${NORMAL}\nDeploy aborted.\n"
     exit 1
 else
-    echo "${NOT_ALRIGHT_MSG}"
+    echo "${ALRIGHT_MSG}"
 fi
 
 echo -n "Merging develop from origin... "
 MERGE_RESULT=$(git merge --no-ff --no-edit origin/develop 2>&1)
 if [[ $? != 0 ]]
 then
-    echo "${ALRIGHT_MSG}"
+    echo "${NOT_ALRIGHT_MSG}"
     echo -e "Error merging, see: \n${MAGENTA}${MERGE_RESULT}${NORMAL}\nDeploy aborted.\n"
     exit 1
 else
-    echo "${NOT_ALRIGHT_MSG}"
+    echo "${ALRIGHT_MSG}"
 fi
 
 echo -n "Linting with warnings allowed... "
 LINT_RESEULT=$(yarn lint 2>&1)
 if [[ $? != 0 ]]
 then
-    echo "${ALRIGHT_MSG}"
+    echo "${NOT_ALRIGHT_MSG}"
     echo -e "Errors while linting, see: \n${MAGENTA}${LINT_RESEULT}${NORMAL}\nDeploy aborted.\n"
     exit 1
 else
-    echo "${NOT_ALRIGHT_MSG}"
+    echo "${ALRIGHT_MSG}"
 fi
 
 echo -n "Running tests... "
 TEST_RESEULT=$(yarn test 2>&1)
 if [[ $? != 0 ]]
 then
-    echo "${ALRIGHT_MSG}"
+    echo "${NOT_ALRIGHT_MSG}"
     echo -e "Tests failed, see: \n${MAGENTA}${TEST_RESEULT}${NORMAL}\nDeploy aborted.\n"
     exit 1
 else
-    echo "${NOT_ALRIGHT_MSG}"
+    echo "${ALRIGHT_MSG}"
 fi
 
 echo -n "Pushing to origin... "
