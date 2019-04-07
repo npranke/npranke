3cfeb10db76d052c8832610ab3b346a29386860f
npranke <nicole@npranke.io>
2019-04-07 18:36:35 -0400

    Add Background component and tests

---
+++ static/js/components/Background.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import BackgroundImg from '../../img/background.jpg'
+
+function Background() {
+    return (
+        <img
+            src={ BackgroundImg }
+            className="background"
+            alt="Conifer in Colorado landscape"
+        />
+    )
+}
+
+export default Background
---
+++ static/js/components/__tests__/Background.test.js
@@ -0,0 +1,25 @@
+import Adapter from 'enzyme-adapter-react-16'
+import Enzyme, { shallow } from 'enzyme'
+import React from 'react'
+
+import Background from '../Background'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('Background', () => {
+    test('has alt attribute', () => {
+        const background = shallow(<Background />)
+
+        expect(
+            background.find('.background').props().alt,
+        ).toBeTruthy()
+    })
+})
+
+describe('Background snapshot', () => {
+    test('matches snapshot', () => {
+        const background = shallow(<Background />)
+
+        expect(background).toMatchSnapshot()
+    })
+})
