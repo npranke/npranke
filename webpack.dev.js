2004fd7b5ebcd83fa68aa4672292b462601cb459
npranke <nicole@npranke.io>
2019-04-07 14:45:41 -0400

    Update webpack.dev.js with dev options for image loaders, use merge.smart()

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const merge = require('webpack-merge')
 
 const commonConfig = require('./webpack.common.js')
@@ -5,6 +7,24 @@ const commonConfig = require('./webpack.common.js')
 const devConfig = {
     devtool: 'eval-source-map',
     mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(gif|png|jp(e)?g|svg)$/,
+                include: path.resolve(__dirname, 'static/img'),
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name]-[hash].[ext]',
+                            publicPath: '/static/dist/',
+                        },
+                    },
+                    'image-webpack-loader',
+                ],
+            },
+        ],
+    },
 }
 
-module.exports = merge(commonConfig, devConfig)
+module.exports = merge.smart(commonConfig, devConfig)
