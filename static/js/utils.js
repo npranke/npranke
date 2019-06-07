c9974835c69122e0b60fee2163ed58b07d57ddd5
npranke <nicole@npranke.io>
2019-06-06 21:27:24 -0400

    Add utils.js with workbookClickHandler()

---
+++ static/js/utils.js
@@ -0,0 +1,10 @@
+export const workbookClickHandler = (e) => {
+    e.preventDefault()
+    alert('Workbook will be here shortly')
+}
+
+const utils = {
+    workbookClickHandler,
+}
+
+export default utils
