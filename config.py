b142b523b43327cbab2f2b2654d371014452a362
npranke <nicole@npranke.io>
2019-06-07 12:58:52 -0400

    Add google analytics snippet to base.html

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ from decouple import config
 
 class AppConfig(object):
     DEBUG = config('DEBUG', default=False, cast=bool)
+    GA = config('GA', default=None)
     TEMPLATE_TAG = config('TEMPLATE_TAG', default=None)
