ea6bda7fac6bf74604facda22caf375020e14fbe
npranke <nicole@npranke.io>
2019-06-07 21:26:36 -0400

    Upgrade clean-webpack-plugin to v3.0.0

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path') // https://nodejs.org/api/
 
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const paths = {
