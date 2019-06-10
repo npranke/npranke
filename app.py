45283b06f068d4c9a0009f4114a23d3e20b15b9b
npranke <nicole@npranke.io>
2019-06-09 21:31:17 -0400

    Add url_for_webpack_asset_processor() context processor, update templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, json, render_template, url_for
 from flask_sslify import SSLify
 
 from config import AppConfig
@@ -7,6 +7,20 @@ app = Flask(__name__)
 app.config.from_object(AppConfig)
 
 sslify = SSLify(app, permanent=True, subdomains=True)
+
+@app.context_processor
+def url_for_webpack_asset_processor():
+    def url_for_webpack_asset(asset_name):
+        with app.open_resource(
+            "static/dist/webpack-assets-manifest.json"
+        ) as file:
+            manifest = json.load(file)
+
+        return url_for(
+            "static",
+            filename="dist/{0}".format(manifest.get(asset_name))
+        )
+    return dict(url_for_webpack_asset=url_for_webpack_asset)
 
 @app.route("/")
 @app.route("/home")
