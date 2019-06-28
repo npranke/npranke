e657d28f9d32656b954a39e8399b049b0504671c
npranke <nicole@npranke.io>
2019-06-28 14:36:06 -0400

    Update App to use WorksheetContainer, use strict, update and add App tests

--- static/js/components/App.jsx
+++ static/js/components/App.jsx
@@ -2,20 +2,24 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Background from './Background'
-import Concentration from './Concentration'
 import Footer from './Footer'
 import Header from './Header'
 import PageNotFound from './PageNotFound'
 import Welcome from './Welcome'
 import Workbook from './Workbook'
+import WorksheetContainer from './WorksheetContainer'
+import worksheets from '../constants/worksheets'
 
 function App() {
+    const { concentration } = worksheets
+
     return (
         <div className="app">
             <Background />
             <Switch>
                 <Route
                     exact
+                    strict
                     path={ [
                         '/',
                         '/home',
@@ -25,20 +29,36 @@ function App() {
                     component={ Header }
                 />
                 <Route
-                    render={
-                        (props) => {
-                            return <Header { ...props } isPageNotFound />
-                        }
-                    }
+                    render={ (props) => {
+                        return <Header { ...props } isPageNotFound />
+                    } }
                 />
             </Switch>
             <Switch>
-                <Route exact path={ ['/', '/home'] } component={ Welcome } />
-                <Route exact path="/workbook" component={ Workbook } />
                 <Route
                     exact
+                    strict
+                    path={ ['/', '/home'] }
+                    component={ Welcome }
+                />
+                <Route
+                    exact
+                    strict
+                    path="/workbook"
+                    component={ Workbook }
+                />
+                <Route
+                    exact
+                    strict
                     path="/workbook/concentration"
-                    component={ Concentration }
+                    render={ (props) => {
+                        return (
+                            <WorksheetContainer
+                                { ...props }
+                                worksheet={ concentration }
+                            />
+                        )
+                    } }
                 />
                 <Route component={ PageNotFound } />
             </Switch>
