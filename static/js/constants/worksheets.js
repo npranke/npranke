e9a1a5dded7257e6e274ba37c092e3d88957e7ed
npranke <nicole@npranke.io>
2019-08-14 23:37:42 -0400

    Add tower to worksheets.js

--- static/js/constants/worksheets.js
+++ static/js/constants/worksheets.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 import Concentration from '@components/concentration/Concentration'
 import ConcentrationInfo from '@components/concentration/ConcentrationInfo'
+import Tower from '@components/tower/Tower'
+import TowerInfo from '@components/tower/TowerInfo'
 
 import ConcentrationIcon from '@img/icon-concentration.png'
+import TowerIcon from '@img/icon-tower.png'
 
 const worksheets = {
     concentration: {
@@ -13,6 +16,14 @@ const worksheets = {
         pathTitle: 'concentration',
         properTitle: 'Concentration',
         title: 'concentration',
+    },
+    tower: {
+        component: <Tower />,
+        icon: TowerIcon,
+        infoComponent: <TowerInfo />,
+        pathTitle: 'tower',
+        properTitle: 'Tower',
+        title: 'tower',
     },
 }
 
