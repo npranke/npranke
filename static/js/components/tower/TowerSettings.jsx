a562cb926db151a89d9b421cb89ec2812ff4ebf9
npranke <nicole@npranke.io>
2019-08-30 22:17:53 -0400

    Update TowerSettings to add settings-inner div, update snapshot

--- static/js/components/tower/TowerSettings.jsx
+++ static/js/components/tower/TowerSettings.jsx
@@ -47,133 +47,135 @@ function TowerSettings(props) {
 
     return (
         <div className="settings">
-            <div className="left-text">
-                <span className="discs-text">
-                    <span className="discs-portion" id="discs-label">
-                        discs:&ensp;
-                    </span>
-                    <span
-                        className="discs-portion"
-                        id="discs-radiogroup"
-                        role="radiogroup"
-                        aria-labelledby="discs-label"
-                    >
-                        <input
-                            name="discs"
-                            type="radio"
-                            id="two-discs"
-                            value="2"
-                            onChange={ discsInputChangeHandler }
-                            checked={ props.discs === 2 }
-                            aria-checked={ props.discs === 2 }
-                        />
-                        <label
-                            className="discs-portion number"
-                            id="two-discs-label"
-                            htmlFor="two-discs"
-                            data-value="2"
-                            ref={ twoDiscs }
-                            onKeyUp={ discsLabelKeyUpHandler }
-                            tabIndex={ props.discs === 2 ? '0' : '-1' }
+            <div className="settings-inner">
+                <div className="left-text">
+                    <span className="discs-text">
+                        <span className="discs-portion" id="discs-label">
+                            discs:&ensp;
+                        </span>
+                        <span
+                            className="discs-portion"
+                            id="discs-radiogroup"
+                            role="radiogroup"
+                            aria-labelledby="discs-label"
                         >
-                            2
-                        </label>
-                        <input
-                            name="discs"
-                            type="radio"
-                            id="three-discs"
-                            value="3"
-                            onChange={ discsInputChangeHandler }
-                            checked={ props.discs === 3 }
-                            aria-checked={ props.discs === 3 }
-                        />
-                        <label
-                            className="discs-portion number"
-                            id="three-discs-label"
-                            htmlFor="three-discs"
-                            data-value="3"
-                            ref={ threeDiscs }
-                            onKeyUp={ discsLabelKeyUpHandler }
-                            tabIndex={ props.discs === 3 ? '0' : '-1' }
-                        >
-                            3
-                        </label>
-                        <input
-                            name="discs"
-                            type="radio"
-                            id="four-discs"
-                            value="4"
-                            onChange={ discsInputChangeHandler }
-                            checked={ props.discs === 4 }
-                            aria-checked={ props.discs === 4 }
-                        />
-                        <label
-                            className="discs-portion number"
-                            id="four-discs-label"
-                            htmlFor="four-discs"
-                            data-value="4"
-                            ref={ fourDiscs }
-                            onKeyUp={ discsLabelKeyUpHandler }
-                            tabIndex={ props.discs === 4 ? '0' : '-1' }
-                        >
-                            4
-                        </label>
-                        <input
-                            name="discs"
-                            type="radio"
-                            id="five-discs"
-                            value="5"
-                            onChange={ discsInputChangeHandler }
-                            checked={ props.discs === 5 }
-                            aria-checked={ props.discs === 5 }
-                        />
-                        <label
-                            className="discs-portion number"
-                            id="five-discs-label"
-                            htmlFor="five-discs"
-                            data-value="5"
-                            ref={ fiveDiscs }
-                            onKeyUp={ discsLabelKeyUpHandler }
-                            tabIndex={ props.discs === 5 ? '0' : '-1' }
-                        >
-                            5
-                        </label>
+                            <input
+                                name="discs"
+                                type="radio"
+                                id="two-discs"
+                                value="2"
+                                onChange={ discsInputChangeHandler }
+                                checked={ props.discs === 2 }
+                                aria-checked={ props.discs === 2 }
+                            />
+                            <label
+                                className="discs-portion number"
+                                id="two-discs-label"
+                                htmlFor="two-discs"
+                                data-value="2"
+                                ref={ twoDiscs }
+                                onKeyUp={ discsLabelKeyUpHandler }
+                                tabIndex={ props.discs === 2 ? '0' : '-1' }
+                            >
+                                2
+                            </label>
+                            <input
+                                name="discs"
+                                type="radio"
+                                id="three-discs"
+                                value="3"
+                                onChange={ discsInputChangeHandler }
+                                checked={ props.discs === 3 }
+                                aria-checked={ props.discs === 3 }
+                            />
+                            <label
+                                className="discs-portion number"
+                                id="three-discs-label"
+                                htmlFor="three-discs"
+                                data-value="3"
+                                ref={ threeDiscs }
+                                onKeyUp={ discsLabelKeyUpHandler }
+                                tabIndex={ props.discs === 3 ? '0' : '-1' }
+                            >
+                                3
+                            </label>
+                            <input
+                                name="discs"
+                                type="radio"
+                                id="four-discs"
+                                value="4"
+                                onChange={ discsInputChangeHandler }
+                                checked={ props.discs === 4 }
+                                aria-checked={ props.discs === 4 }
+                            />
+                            <label
+                                className="discs-portion number"
+                                id="four-discs-label"
+                                htmlFor="four-discs"
+                                data-value="4"
+                                ref={ fourDiscs }
+                                onKeyUp={ discsLabelKeyUpHandler }
+                                tabIndex={ props.discs === 4 ? '0' : '-1' }
+                            >
+                                4
+                            </label>
+                            <input
+                                name="discs"
+                                type="radio"
+                                id="five-discs"
+                                value="5"
+                                onChange={ discsInputChangeHandler }
+                                checked={ props.discs === 5 }
+                                aria-checked={ props.discs === 5 }
+                            />
+                            <label
+                                className="discs-portion number"
+                                id="five-discs-label"
+                                htmlFor="five-discs"
+                                data-value="5"
+                                ref={ fiveDiscs }
+                                onKeyUp={ discsLabelKeyUpHandler }
+                                tabIndex={ props.discs === 5 ? '0' : '-1' }
+                            >
+                                5
+                            </label>
+                        </span>
                     </span>
-                </span>
-            </div>
-            <div className="right-text">
-                <span className="moves-text">
-                    <span className="moves-portion">
-                        moves:&ensp;
+                </div>
+                <div className="right-text">
+                    <span className="moves-text">
+                        <span className="moves-portion">
+                            moves:&ensp;
+                        </span>
+                        <span className="moves-portion number">
+                            {
+                                props.moves < 10
+                                    ? `0${props.moves}`
+                                    : `${props.moves}`
+                            }
+                        </span>
                     </span>
-                    <span className="moves-portion number">
-                        {
-                            props.moves < 10
-                                ? `0${props.moves}`
-                                : `${props.moves}`
-                        }
+                    <span className="time-text">
+                        <span className="time-portion">
+                            time:&emsp;
+                        </span>
+                        <span className="time-portion number">
+                            { minutes }
+                        </span>
+                        <span className="time-portion">
+                            &ensp;:&ensp;
+                        </span>
+                        <span className="time-portion number">
+                            { seconds }
+                        </span>
+                        <span className="time-portion">
+                            &ensp;:&ensp;
+                        </span>
+                        <span className="time-portion number">
+                            { centiseconds }
+                        </span>
                     </span>
-                </span>
-                <span className="time-text">
-                    <span className="time-portion">
-                        time:&emsp;
-                    </span>
-                    <span className="time-portion number">
-                        { minutes }
-                    </span>
-                    <span className="time-portion">
-                        &ensp;:&ensp;
-                    </span>
-                    <span className="time-portion number">
-                        { seconds }
-                    </span>
-                    <span className="time-portion">
-                        &ensp;:&ensp;
-                    </span>
-                    <span className="time-portion number">
-                        { centiseconds }
-                    </span>
-                </span>
+                </div>
             </div>
         </div>
     )
