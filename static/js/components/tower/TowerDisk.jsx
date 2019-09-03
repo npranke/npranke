88b8c6a69791b30bfaf051929a609341a4bbfbdf
npranke <nicole@npranke.io>
2019-09-02 21:24:09 -0400

    Add TowerDisk component and tests

---
+++ static/js/components/tower/TowerDisk.jsx
@@ -0,0 +1,66 @@
+import { DragSource } from 'react-dnd'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+export const spec = {
+    beginDrag: (props) => {
+        return { diskid: props.diskid }
+    },
+    endDrag: (props, monitor) => {
+        if (monitor.didDrop()) {
+            const result = monitor.getDropResult()
+
+            props.moveDisk(props.diskid, props.tower, result.toTower)
+        }
+    },
+    canDrag: (props) => {
+        return (
+            !props.isComplete
+            && props.diskids.indexOf(props.diskid) === props.diskids.length - 1
+        )
+    },
+}
+
+export function collect(connect, monitor) {
+    return {
+        connectDragSource: connect.dragSource(),
+        canDrag: monitor.canDrag(),
+        isDragging: monitor.isDragging(),
+    }
+}
+
+export function TowerDisk(props) {
+    return props.connectDragSource(
+        <div
+            className={
+                'disk'
+                + `${props.canDrag ? ' candrag' : ''}`
+                + `${props.isDragging ? ' dragging' : ''}`
+            }
+        >
+            { props.image }
+        </div>,
+    )
+}
+
+TowerDisk.defaultProps = {
+    canDrag: false,
+    connectDragSource: undefined,
+    diskids: [],
+    isComplete: false,
+    isDragging: false,
+}
+
+TowerDisk.propTypes = {
+    canDrag: PropTypes.bool,
+    connectDragSource: PropTypes.func,
+    diskid: PropTypes.string.isRequired,
+    diskids: PropTypes.arrayOf(PropTypes.string),
+    image: PropTypes.node.isRequired,
+    isComplete: PropTypes.bool,
+    isDragging: PropTypes.bool,
+    moveDisk: PropTypes.func.isRequired,
+    tower: PropTypes.string.isRequired,
+}
+
+export default DragSource('disk', spec, collect)(TowerDisk)
