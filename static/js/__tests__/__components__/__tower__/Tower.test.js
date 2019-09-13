6ccbbe90ea67da33f7b4c2c728db81acf382bb2e
npranke <nicole@npranke.io>
2019-09-13 12:32:50 -0400

    Update Tower component and tests, add more tests

--- static/js/__tests__/__components__/__tower__/Tower.test.js
+++ static/js/__tests__/__components__/__tower__/Tower.test.js
@@ -19,29 +19,269 @@ describe('Tower', () => {
         ).toBe(true)
     })
 
-    test('updateDiscs() sets updated discs', () => {
+    test('has one locations container', () => {
         const tower = shallow(<Tower />)
 
-        tower.instance().updateDiscs('5')
+        expect(
+            tower.find(
+                '.tower .locations-wrapper .locations-container',
+            ).length,
+        ).toBe(1)
+    })
+
+    test('has three location sections', () => {
+        const tower = shallow(<Tower />)
 
         expect(
-            tower.state('discs'),
-        ).toBe(5)
+            tower.find(
+                '.tower .location-wrapper TowerLocation',
+            ).length,
+        ).toBe(3)
+    })
+
+    describe('towers', () => {
+        test('has four towers', () => {
+            const tower = shallow(<Tower />)
+
+            expect(
+                Object.keys(tower.instance().towers),
+            ).toHaveLength(4)
+        })
+
+        test('each tower has link', () => {
+            const tower = shallow(<Tower />)
+            const { towers } = tower.instance()
+
+            Object.keys(towers).forEach((towerKey) => {
+                expect(
+                    towers[towerKey],
+                ).toHaveProperty('link', expect.any(String))
+            })
+        })
+
+        test('each tower has clickHandler', () => {
+            const tower = shallow(<Tower />)
+            const { towers } = tower.instance()
+
+            Object.keys(towers).forEach((towerKey) => {
+                expect(
+                    towers[towerKey],
+                ).toHaveProperty('clickHandler', expect.any(Function))
+            })
+        })
+
+        test('each tower has landscape disks', () => {
+            const tower = shallow(<Tower />)
+            const { towers } = tower.instance()
+
+            Object.keys(towers).forEach((towerKey) => {
+                expect(
+                    towers[towerKey],
+                ).toHaveProperty('landscape', expect.any(Array))
+                expect(
+                    towers[towerKey].landscape.length,
+                ).toBe(parseInt(towerKey, 10))
+
+                towers[towerKey].landscape.forEach((disk) => {
+                    expect(
+                        parseInt(disk.diskid, 10),
+                    ).toBeLessThan(parseInt(towerKey, 10) + 1)
+                    expect(
+                        disk.image,
+                    ).toBeTruthy()
+                })
+            })
+        })
+
+        test('each tower has portrait disks', () => {
+            const tower = shallow(<Tower />)
+            const { towers } = tower.instance()
+
+            Object.keys(towers).forEach((towerKey) => {
+                expect(
+                    towers[towerKey],
+                ).toHaveProperty('portrait', expect.any(Array))
+                expect(
+                    towers[towerKey].landscape.length,
+                ).toBe(parseInt(towerKey, 10))
+
+                towers[towerKey].landscape.forEach((disk) => {
+                    expect(
+                        parseInt(disk.diskid, 10),
+                    ).toBeLessThan(parseInt(towerKey, 10) + 1)
+                    expect(
+                        disk.image,
+                    ).toBeTruthy()
+                })
+            })
+        })
+    })
+
+    describe('originTimeout in component lifecycle', () => {
+        beforeAll(() => {
+            jest.useFakeTimers()
+
+            window.clearTimeout = jest.spyOn(window, 'clearTimeout')
+        })
+
+        afterEach(() => {
+            jest.clearAllTimers()
+
+            window.clearTimeout.mockClear()
+        })
+
+        afterAll(() => {
+            window.clearTimeout.mockRestore()
+        })
+
+        test('originTimeout is defined when component did mount', () => {
+            const tower = shallow(<Tower />, { disableLifecycleMethods: true })
+
+            expect(
+                tower.instance().originTimeout,
+            ).not.toBeDefined()
+
+            tower.instance().componentDidMount()
+
+            expect(
+                tower.instance().originTimeout,
+            ).toBeDefined()
+        })
+
+        test('originTimeout sets origin when component did mount', () => {
+            const tower = shallow(<Tower />, { disableLifecycleMethods: true })
+
+            expect(
+                tower.state('origin'),
+            ).toEqual([])
+
+            tower.instance().componentDidMount()
+
+            jest.advanceTimersByTime(400)
+
+            expect(
+                tower.state('origin'),
+            ).toEqual(['3', '2', '1'])
+        })
+
+        test('originTimeout is cleared when component will unmount', () => {
+            const tower = shallow(<Tower />)
+
+            tower.instance().componentWillUnmount()
+
+            expect(window.clearTimeout).toHaveBeenCalledTimes(1)
+        })
+    })
+
+    describe('updateDisks()', () => {
+        test('sets updated disks', () => {
+            const tower = shallow(<Tower />)
+
+            tower.instance().updateDisks('5')
+
+            expect(
+                tower.state('disks'),
+            ).toBe(5)
+        })
+
+        test('sets originTimeout', () => {
+            const tower = shallow(<Tower />)
+
+            tower.instance().updateDisks('2')
+
+            expect(
+                tower.instance().originTimeout,
+            ).toBeDefined()
+        })
+
+        test('originTimeout resets origin with updated diskids', () => {
+            jest.useFakeTimers()
+
+            const tower = shallow(<Tower />)
+
+            tower.instance().updateDisks('4')
+
+            jest.advanceTimersByTime(200)
+
+            expect(
+                tower.state('origin'),
+            ).toEqual(['4', '3', '2', '1'])
+        })
+    })
+
+    describe('moveDisk()', () => {
+        test('moves disk when not completing tower', () => {
+            const tower = shallow(<Tower />)
+
+            tower.setState({
+                isTimeRunning: true,
+                moves: 2,
+                disks: 3,
+                origin: ['3'],
+                buffer: ['2'],
+                target: ['1'],
+            })
+
+            tower.instance().moveDisk('1', 'target', 'buffer')
+
+            expect(tower.state()).toMatchObject({
+                isComplete: false,
+                isTimeRunning: true,
+                moves: 3,
+                origin: ['3'],
+                buffer: ['2', '1'],
+                target: [],
+            })
+        })
+
+        test('moves disk when completing tower', () => {
+            const tower = shallow(<Tower />)
+
+            tower.setState({
+                isTimeRunning: true,
+                moves: 6,
+                disks: 3,
+                origin: ['1'],
+                buffer: [],
+                target: ['3', '2'],
+            })
+
+            tower.instance().moveDisk('1', 'origin', 'target')
+
+            expect(tower.state()).toMatchObject({
+                isComplete: true,
+                isTimeRunning: false,
+                moves: 7,
+                origin: [],
+                buffer: [],
+                target: ['3', '2', '1'],
+            })
+        })
     })
 
     describe('sending tower events', () => {
         beforeAll(() => {
-            utils.sendEvent = jest.fn()
+            utils.getSendEventHandler = jest.spyOn(
+                utils,
+                'getSendEventHandler',
+            )
+            utils.sendEvent = jest.spyOn(utils, 'sendEvent')
         })
 
         afterEach(() => {
-            utils.sendEvent.mockReset()
+            utils.getSendEventHandler.mockClear()
+            utils.sendEvent.mockClear()
         })
 
-        test('when discs is updated', () => {
+        afterAll(() => {
+            utils.getSendEventHandler.mockRestore()
+            utils.sendEvent.mockRestore()
+        })
+
+        test('when disks is updated', () => {
             const tower = shallow(<Tower />)
 
-            tower.instance().updateDiscs('2')
+            tower.instance().updateDisks('2')
 
             expect(
                 utils.sendEvent,
@@ -49,7 +289,70 @@ describe('Tower', () => {
                 1,
                 'tower',
                 'select',
-                'discs-2',
+                'disks-2',
+            )
+        })
+
+        test('when disk is moved', () => {
+            const tower = shallow(<Tower />)
+
+            tower.setState({
+                disks: 4,
+                origin: ['4', '3', '2', '1'],
+                buffer: [],
+                target: [],
+            })
+
+            tower.instance().moveDisk('1', 'origin', 'buffer')
+
+            expect(
+                utils.sendEvent,
+            ).toHaveBeenNthCalledWith(
+                1,
+                'tower',
+                'move',
+                'disk',
+            )
+        })
+
+        test('when tower is completed', () => {
+            const tower = shallow(<Tower />)
+
+            tower.setState({
+                isTimeRunning: true,
+                moves: 6,
+                disks: 3,
+                origin: ['1'],
+                buffer: [],
+                target: ['3', '2'],
+            })
+
+            tower.instance().moveDisk('1', 'origin', 'target')
+
+            expect(
+                utils.sendEvent,
+            ).toHaveBeenNthCalledWith(
+                2,
+                'tower',
+                'complete',
+                'disks-3',
+                7,
+            )
+        })
+
+        test('with getSendEventHandler for completed tower', () => {
+            shallow(<Tower />)
+
+            expect(
+                utils.getSendEventHandler,
+            ).toHaveBeenCalledTimes(4)
+            expect(
+                utils.getSendEventHandler,
+            ).toHaveBeenNthCalledWith(
+                1,
+                'tower',
+                'navigate',
+                'PIA22931',
             )
         })
     })
--- static/js/components/tower/Tower.jsx
+++ static/js/components/tower/Tower.jsx
@@ -1,42 +1,194 @@
+import { DragDropContext } from 'react-dnd'
+import HTML5toTouch from 'react-dnd-multi-backend/lib/HTML5toTouch'
+import MultiBackend from 'react-dnd-multi-backend'
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import TowerLocation from '@components/tower/TowerLocation'
 import TowerSettings from '@components/tower/TowerSettings'
 import withPortraitListener from '@components/hoc/PortraitListener'
 
-import { sendEvent } from '@utils'
+import { LAYOUTS, LOCATIONS, TOWER_SOURCES } from '@constants/tower'
+
+import { getSendEventHandler, sendEvent } from '@utils'
 
 export class Tower extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
+            isComplete: false,
             isTimeRunning: false,
+            shouldTimeReset: false,
             moves: 0,
-            discs: 3,
+            disks: 3,
+            origin: [],
+            buffer: [],
+            target: [],
         }
 
-        this.updateDiscs = this.updateDiscs.bind(this)
+        this.moveDisk = this.moveDisk.bind(this)
+        this.updateDisks = this.updateDisks.bind(this)
+
+        this.towers = Object.keys(TOWER_SOURCES).reduce(
+            (towerAccumulator, tower) => {
+                towerAccumulator[tower] = {
+                    clickHandler: getSendEventHandler(
+                        'tower',
+                        'navigate',
+                        `PIA${TOWER_SOURCES[tower].sourceid}`,
+                    ),
+                    link: (
+                        'https://www.jpl.nasa.gov/spaceimages/details.php?'
+                        + `id=PIA${TOWER_SOURCES[tower].sourceid}`
+                    ),
+                }
+
+                LAYOUTS.forEach((layout) => {
+                    towerAccumulator[tower][layout] = TOWER_SOURCES[
+                        tower
+                    ].disks[layout].map((disksource, index) => {
+                        const description = (
+                            'NASA/JPL Juno PIA'
+                            + `${TOWER_SOURCES[tower].sourceid}, `
+                            + `disk ${index + 1} of ${tower}`
+                        )
+
+                        return {
+                            diskid: `${index + 1}`,
+                            image: (
+                                <img
+                                    src={ disksource }
+                                    className="disk-image"
+                                    alt={ description }
+                                    title={ description }
+                                />
+                            ),
+                        }
+                    })
+                })
+
+                return towerAccumulator
+            },
+            {},
+        )
     }
 
-    updateDiscs(discs) {
-        const updatedDiscs = parseInt(discs, 10)
+    componentDidMount() {
+        this.originTimeout = setTimeout(() => {
+            this.setState((state) => {
+                return {
+                    origin: Array.from(
+                        { length: state.disks },
+                        (value, index) => { return `${index + 1}` },
+                    ).reverse(),
+                }
+            })
+        }, 400)
+    }
 
-        sendEvent('tower', 'select', `discs-${updatedDiscs}`)
+    componentWillUnmount() {
+        clearTimeout(this.originTimeout)
+    }
 
-        this.setState({ discs: updatedDiscs })
+    moveDisk(diskid, fromLocation, toLocation) {
+        this.setState((state) => {
+            const fromLocationDisks = state[fromLocation].filter(
+                (fromDisk) => {
+                    return fromDisk !== diskid
+                },
+            )
+            const toLocationDisks = state[toLocation].concat([diskid])
+            const isComplete = (
+                toLocation === 'target'
+                && toLocationDisks.length === state.disks
+            )
+            const isTimeRunning = !isComplete
+
+            sendEvent('tower', 'move', 'disk')
+
+            if (isComplete) {
+                sendEvent(
+                    'tower',
+                    'complete',
+                    `disks-${state.disks}`,
+                    state.moves + 1,
+                )
+            }
+
+            return {
+                isComplete,
+                isTimeRunning,
+                shouldTimeReset: false,
+                moves: state.moves + 1,
+                [fromLocation]: fromLocationDisks,
+                [toLocation]: toLocationDisks,
+            }
+        })
+    }
+
+    updateDisks(disks) {
+        const updatedDisks = parseInt(disks, 10)
+
+        sendEvent('tower', 'select', `disks-${updatedDisks}`)
+
+        this.setState(() => {
+            this.originTimeout = setTimeout(() => {
+                this.setState({
+                    origin: Array.from(
+                        { length: updatedDisks },
+                        (value, index) => { return `${index + 1}` },
+                    ).reverse(),
+                })
+            }, 200)
+
+            return {
+                isComplete: false,
+                isTimeRunning: false,
+                shouldTimeReset: true,
+                moves: 0,
+                disks: updatedDisks,
+                origin: [],
+                buffer: [],
+                target: [],
+            }
+        })
     }
 
     render() {
+        const towerLocations = LOCATIONS.map((location) => {
+            return (
+                <React.Fragment key={ location }>
+                    <div className="location-wrapper">
+                        <TowerLocation
+                            location={ location }
+                            diskids={ this.state[location] }
+                            disks={ this.state.disks }
+                            tower={ this.towers[this.state.disks] }
+                            moveDisk={ this.moveDisk }
+                            isComplete={ this.state.isComplete }
+                            isPortrait={ this.props.isPortrait }
+                        />
+                    </div>
+                </React.Fragment>
+            )
+        })
+
         return (
             <div className="tower">
                 <div className="settings-wrapper">
                     <TowerSettings
-                        discs={ this.state.discs }
+                        disks={ this.state.disks }
                         isTimeRunning={ this.state.isTimeRunning }
+                        shouldTimeReset={ this.state.shouldTimeReset }
                         moves={ this.state.moves }
-                        updateDiscs={ this.updateDiscs }
+                        updateDisks={ this.updateDisks }
                     />
+                </div>
+                <div className="locations-wrapper">
+                    <div className="locations-container">
+                        { towerLocations }
+                    </div>
                 </div>
             </div>
         )
@@ -51,4 +203,6 @@ Tower.propTypes = {
     isPortrait: PropTypes.bool,
 }
 
-export default withPortraitListener(Tower)
+export default DragDropContext(MultiBackend(HTML5toTouch))(
+    withPortraitListener(Tower),
+)
