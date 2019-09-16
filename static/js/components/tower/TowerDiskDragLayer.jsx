f9abca86a5f78417eff48ddc84e365d53bbb50c8
npranke <nicole@npranke.io>
2019-09-16 00:20:50 -0400

    Add TowerDiskDragLayer component and tests

---
+++ static/js/components/tower/TowerDiskDragLayer.jsx
@@ -0,0 +1,62 @@
+import { DragLayer } from 'react-dnd'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+export function collect(monitor) {
+    return {
+        sourceClientOffset: monitor.getSourceClientOffset(),
+    }
+}
+
+export function TowerDiskDragLayer(props) {
+    const previewStyle = props.sourceClientOffset ? {
+        height: props.height,
+        width: props.width,
+        transform: (
+            'translate('
+            + `${props.sourceClientOffset.x}px, `
+            + `${props.sourceClientOffset.y}px`
+            + ')'
+        ),
+    } : { display: 'none' }
+
+    return (
+        <div
+            className="disk-drag-layer"
+            style={ {
+                pointerEvents: 'none',
+                position: 'fixed',
+                zIndex: 10,
+                left: 0,
+                top: 0,
+                width: '100%',
+                height: '100%',
+            } }
+        >
+            <div
+                className="disk dragging-preview"
+                style={ previewStyle }
+            >
+                { props.image }
+            </div>
+        </div>
+    )
+}
+
+TowerDiskDragLayer.defaultProps = {
+    height: 0,
+    sourceClientOffset: null,
+    width: 0,
+}
+
+TowerDiskDragLayer.propTypes = {
+    height: PropTypes.number,
+    image: PropTypes.node.isRequired,
+    sourceClientOffset: PropTypes.shape({
+        x: PropTypes.number,
+        y: PropTypes.number,
+    }),
+    width: PropTypes.number,
+}
+
+export default DragLayer(collect)(TowerDiskDragLayer)
