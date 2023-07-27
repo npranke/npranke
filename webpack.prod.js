dfb8293210a555db3b5d00e6df363bec0ac303e9
npranke <nicole@npranke.io>
2023-07-27 15:34:39 -0400

    Replace UglifyJSPlugin with TerserPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,18 @@
 const { merge } = require('webpack-merge')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require("terser-webpack-plugin")
 const webpack = require('webpack')
 
 const commonConfig = require('./webpack.common.js')
 
 const prodConfig = {
     mode: 'production',
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({ extractComments: false })],
+    },
     plugins: [
         new OptimizeCSSAssetsPlugin(),
-        new UglifyJSPlugin(),
         new webpack.EnvironmentPlugin({ GA: null }),
     ],
 }
