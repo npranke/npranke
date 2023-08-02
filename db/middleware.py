179703b3ede866a039cf43590272359b46a75666
npranke <nicole@npranke.io>
2023-08-02 14:33:22 -0400

    Add type hints to middleware.py

--- db/middleware.py
+++ db/middleware.py
@@ -1,10 +1,12 @@
-from tinydb import TinyDB
+from typing import Any, Dict
+
 from tinydb.middlewares import Middleware
+from tinydb.storages import JSONStorage, Storage
 
 
 class ReadOnlyMiddleware(Middleware):
-    def __init__(self, storage_cls=TinyDB.default_storage_class):
+    def __init__(self, storage_cls: Middleware | type[Storage]=JSONStorage):
         super(ReadOnlyMiddleware, self).__init__(storage_cls)
 
-    def write(self, data):
+    def write(self, data: Dict[str, Dict[str, Any]]):
         raise RuntimeError("ReadOnlyMiddleware write not allowed")
