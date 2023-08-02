2636f56cff66e87e61b8983be28e15637fc114e7
npranke <nicole@npranke.io>
2023-08-02 14:45:06 -0400

    Add pyright to staging deploy

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -64,7 +64,7 @@ else
 fi
 
 echo -n "Linting python with ruff... "
-RUFF_RESULT=$(pipenv run ruff check --show-files . 2>&1)
+RUFF_RESULT=$(pipenv run ruff check . 2>&1)
 if [[ $? != 0 ]]
 then
     echo "${NOT_ALRIGHT_MSG}"
@@ -74,7 +74,18 @@ else
     echo "${ALRIGHT_MSG}"
 fi
 
-echo -n "Linting js, warnings allowed... "
+echo -n "Type checking python with pyright... "
+PYRIGHT_RESULT=$(pipenv run yarn run pyright 2>&1)
+if [[ $? != 0 ]]
+then
+    echo "${NOT_ALRIGHT_MSG}"
+    echo -e "Errors while type checking, see: \n${MAGENTA}${PYRIGHT_RESULT}${NORMAL}\nDeploy aborted.\n"
+    exit 1
+else
+    echo "${ALRIGHT_MSG}"
+fi
+
+echo -n "Linting javascript, warnings allowed... "
 ESLINT_RESULT=$(yarn lint 2>&1)
 if [[ $? != 0 ]]
 then
