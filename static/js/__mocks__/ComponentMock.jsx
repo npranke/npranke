b16b2fed7e79b355bdf1f659632ce3a2c459f48e
npranke <nicole@npranke.io>
2023-08-29 13:12:39 -0500

    Update ComponentMock with document role and props content

--- static/js/__mocks__/ComponentMock.jsx
+++ static/js/__mocks__/ComponentMock.jsx
@@ -1,7 +1,10 @@
-function ComponentMock() {
+function ComponentMock(props) {
     return (
-        <div className="component-mock">
-            mock
+        <div className="component-mock" role="document">
+            {
+                `${Object.keys(props).toString()} `
+                + `${Object.values(props).toString()}`
+            }
         </div>
     )
 }
