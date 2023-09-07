af2c5b95baaa2c783873a4faa9640455bda6bec2
npranke <nicole@npranke.io>
2023-09-06 22:42:27 -0500

    Add data-testids to ComponentUseOffsetListenerMock, update snapshot

--- static/js/__mocks__/ComponentUseOffsetListenerMock.jsx
+++ static/js/__mocks__/ComponentUseOffsetListenerMock.jsx
@@ -7,8 +7,12 @@ function ComponentUseOffsetListenerMock(props) {
 
     return (
         <div className="component-use-offset-listener-mock">
-            <div className="height">{ height }</div>
-            <div className="width">{ width }</div>
+            <div className="height" data-testid="height-div-elem">
+                { height }
+            </div>
+            <div className="width" data-testid="width-div-elem">
+                { width }
+            </div>
         </div>
     )
 }
